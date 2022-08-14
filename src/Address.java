cca5a654ceec245f298bbe919eddcfbd3bb7022d
marklen <marklen86@gmail.com>
2022-08-13 23:52:10 -0500

    Address, Customer created

---
+++ src/Address.java
@@ -0,0 +1,37 @@
+
+
+public class Address {
+    private String streetNumber;
+    private String streetName;
+    private String additionalAddressLine;
+    private String zipCode;
+    private String state;
+
+    public Address(String streetNumber, String streetName, String additionalAddressLine, String zipCode, String state) {
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.additionalAddressLine = additionalAddressLine;
+        this.zipCode = zipCode;
+        this.state = state;
+    }
+
+    public String getStreetNumber() {
+        return this.streetNumber;
+    }
+
+    public String getStreetName() {
+        return this.streetName;
+    }
+
+    public String getAdditionalAddressLine() {
+        return this.additionalAddressLine;
+    }
+
+    public String getZipCode() {
+        return this.zipCode;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+}
---
+++ src/Customer.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+    private String userName;
+    private String email;
+    private List<Address> address;
+
+    public Customer(UUID id, String userName, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public Customer(UUID id, String userName, String email, List<Address> address) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public List<Address> getAddress() {
+        return this.address;
+    }
+
+    public String toString() {
+        return "Customer{id=" + this.id + ", userName='" + this.userName + "', email='" + this.email + "', address=" + this.address + "}";
+    }
+}
