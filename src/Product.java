152fc1351b2156011d1ccd883e35a391ae687c25
marklen <marklen86@gmail.com>
2022-08-14 00:11:47 -0500

    Product created

---
+++ src/Product.java
@@ -0,0 +1,45 @@
+
+import java.util.UUID;
+
+public class Product {
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer stock;
+    private Integer remainingStock;
+    private UUID categoryId;
+
+    public Product(UUID id, String name, Double price, Integer stock, Integer remainingStock, UUID categoryId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.remainingStock = remainingStock;
+        this.categoryId = categoryId;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public Integer getStock() {
+        return this.stock;
+    }
+
+    public Integer getRemainingStock() {
+        return this.remainingStock;
+    }
+
+    public UUID getCategoryId() {
+        return this.categoryId;
+    }
+}
+
