34518d88147e04adecd825a2aa9fc533750d8f01
marklen <marklen86@gmail.com>
2022-08-14 00:10:58 -0500

    Discounts added

---
+++ src/discount/AmountBasedDiscount.java
@@ -0,0 +1,26 @@
+package discount;
+
+import java.util.UUID;
+
+public class AmountBasedDiscount extends Discount {
+
+private Double discountAmount;
+
+
+    public AmountBasedDiscount(UUID id, String name, Double thresholdAmount, Double discountAmount) {
+        super(id, name, thresholdAmount);
+        this.discountAmount = discountAmount;
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    @Override
+    public Double calculateCartAmountDiscountApplied(Double amount) {
+        return null;
+    }
+
+
+
+}
