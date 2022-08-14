eaff338b0c1ca272024696bacc951c27be01c7da
marklen <marklen86@gmail.com>
2022-08-14 00:10:30 -0500

    Balances added

---
+++ src/balance/CustomerBalance.java
@@ -0,0 +1,16 @@
+package balance;
+
+import balance.Balance;
+
+import java.util.UUID;
+
+public class CustomerBalance extends Balance {
+    public CustomerBalance(UUID customerID, Double balance) {
+        super(customerID, balance);
+    }
+
+    public Double addBalance(Double additionalBalance) {
+        this.setBalance(this.getBalance() + additionalBalance);
+        return this.getBalance();
+    }
+}
---
+++ src/balance/GiftCardBalance.java
@@ -0,0 +1,18 @@
+package balance;
+
+import balance.Balance;
+
+import java.util.UUID;
+
+public class GiftCardBalance extends Balance {
+    public GiftCardBalance(UUID customerID, Double balance) {
+        super(customerID, balance);
+    }
+
+    @Override
+    public Double addBalance(Double additionalBalance) {
+        double promotionAmount = additionalBalance * 10/100;
+        setBalance(getBalance()+additionalBalance+promotionAmount);
+        return getBalance();
+    }
+}
